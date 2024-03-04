f38e10c9611fe3fbc7c1b093fecd45891b38b99d
AngusGaukrogerDev <agaukroger77@gmail.com>
2024-03-04 14:11:40 +0000

    Contact section changes

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -16,28 +16,48 @@ const Contact = () => {
       } = useForm<Inputs>();
       const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
     return(
-        <section id="contact" className="w-full h-auto flex flex-col justify-center items-center gap-10 bg-pitahaya-white text-center">   
-            <div className="flex flex-col justify-center items-center gap-5">
-                <h2 className="text-pitahaya-black">Contact</h2>
-                <h3 className="text-pitahaya-grey">Book a call with us today! Or send a message via the contact form.</h3>
-            </div>
-            
-            <div className="w-full flex flex-col justify-center items-center gap-5 px-5">
+        <section id="contact" className="w-full h-auto flex flex-col lg:flex-row justify-center items-center gap-12 bg-pitahaya-white text-center my-5 px-3">   
+            {/* <div className="flex flex-col justify-center items-center gap-5 w-full px-5 b"> */}
+                
+                {/* <h3 className="text-pitahaya-grey">Book a call with us today! Or send a message via the contact form.</h3> */}
+            {/* </div>   */}
+            <div className="w-full h-full flex flex-col justify-center items-center gap-5 px-3 py-5 border-2 border-pitahaya-grey-900 rounded-md">
+                <h2 className="text-pitahaya-black">Book a call with us...</h2>
                 <img src="https://dummyimage.com/300x100/808080/F8FBFC.jpg&text=Pitahaya+Labs+logo" />
                 <a target="_blank" href="https://cnnct.uk/pitahayalabs/initial-learning-platform-discussion" >
-                    <button className="bg-pitahaya-black text-pitahaya-white px-4 py-2 rounded-md w-36 sm:w-56 md:w-56 h-14  hover:cursor-pointer hover:bg-pitahaya-yellow">
+                    <button className="bg-pitahaya-yellow text-pitahaya-white px-4 py-2 rounded-md w-36 sm:w-56 md:w-56 h-14  hover:cursor-pointer hover:bg-pitahaya-yellow">
                         <h3>Book a call</h3>
                     </button>                  
                 </a>
             </div>
             <form 
-                className="bg-pitahaya-beige h-full w-10/12 flex flex-col justify-center items-center gap-5 px-5 py-5 rounded-md"
+                className="border-2 h-full w-full flex flex-col justify-center items-center gap-5 px-3 py-5 rounded-md border-pitahaya-grey-900"
                 onSubmit={handleSubmit(onSubmit)}
             >
-                <input className="bg-pitahaya-white w-full rounded-md h-8" {...register("name", { required: true })} />
-                <input className="bg-pitahaya-white w-full rounded-md h-8" {...register("email", { required: true })} />
-                <input className="bg-pitahaya-white w-full rounded-md h-24" {...register("message", { required: true })} />
+                <h2>Or send us a message</h2>
+                <div className="flex flex-col justify-center items-start gap-2 w-full">
+                    <div className="flex flex-row justify-center items-center gap-1">
+                        <label className="ml-2 text-pitahaya-black" htmlFor="name">Name</label>
+                        <label className="text-pitahaya-yellow">*</label>
+                    </div>
+                    <input className="bg-pitahaya-white w-full rounded-md h-12 border-2 border-pitahaya-grey pl-2" type="text" placeholder="What's your name?" {...register("name", { required: true })} />
+                </div>
+                <div className="flex flex-col justify-center items-start gap-2 w-full">
+                    <div className="flex flex-row justify-center items-center gap-1">
+                        <label className="ml-2 text-pitahaya-black" htmlFor="email">Email</label>
+                        <label className="text-pitahaya-yellow">*</label>
+                    </div>
+                    <input className="bg-pitahaya-white w-full rounded-md h-12 border-2 border-pitahaya-grey pl-2" type="email" placeholder="What's your email?" {...register("email", { required: true })} />
+                </div>
+                <div className="flex flex-col justify-center items-start gap-2 w-full">
+                    <div className="flex flex-row justify-center items-center gap-1">
+                        <label className="ml-2 text-pitahaya-black" htmlFor="message">Message</label>
+                        <label className="text-pitahaya-yellow">*</label>
+                    </div>
+                    <textarea className="bg-pitahaya-white w-full rounded-md h-36 border-2 border-pitahaya-grey pl-2 pt-1" placeholder="What would you like to tell us?" {...register("message", { required: true })} />
+                </div>
                 <input className="bg-pitahaya-black text-pitahaya-white px-4 py-2 rounded-md w-36 sm:w-56 md:w-56 h-14  hover:cursor-pointer hover:bg-pitahaya-yellow" type="submit" />
+
             </form>
         </section>
     );
