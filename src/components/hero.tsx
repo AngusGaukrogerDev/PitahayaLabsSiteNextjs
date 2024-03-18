9672b6708f203ace354d02dc1393ef50374604fa
AngusGaukrogerDev <agaukroger77@gmail.com>
2024-03-18 10:22:01 -0500

    hyphen...

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
             <section id="home" className="hero-image w-full h-screen flex flex-col justify-center items-center pt-10">
                 
                 <div className="h-screen w-full xl:w-2/3 px-5 md:px-28 lg:px-32 xl:px-44 gap-4 xl:gap-7 flex flex-col justify-center items-center text-center sm:text-center sm:items-center">
-                    <h1 className="text-pitahaya-black ">Software Development Services for EdTech Startups</h1>
+                    <h1 className="text-pitahaya-black ">Software Development Services for EdTech Start-ups</h1>
                     <h3 className="text-pitahaya-grey w-10/12 ">We specialise in building bespoke learning platforms for EdTech Startups, harnessing modern technologies and AI.</h3>
                     <Link legacyBehavior href="/#contact" as="/#contact" >
                         <button className=" bg-pitahaya-black text-pitahaya-white px-4 py-2  rounded-md w-36 sm:w-56 md:w-56 h-14  hover:cursor-pointer transition duration-300 transform hover:scale-110">
