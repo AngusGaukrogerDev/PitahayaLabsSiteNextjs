4a9039ac9296c5c3d43f4b4833d9db96a80461f9
AngusGaukrogerDev <agaukroger77@gmail.com>
2024-03-18 18:17:18 -0500

    Text tweak

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -20,7 +20,7 @@ const Hero = () => {
                 </div>
                 <div className="h-screen w-full xl:w-2/3 px-5 md:px-28 lg:px-32 xl:px-44 gap-3 xl:gap-7 flex flex-col justify-center items-center text-center sm:text-center sm:items-center">
                     <h1 className="text-pitahaya-black hidden">Software Development Services for EdTech Start-ups</h1>
-                    <h2 className="text-pitahaya-black text-4xl xl:text-6xl">Software Development Services for EdTech Start-ups</h2>
+                    <h2 className="text-pitahaya-black text-4xl lg:text-5xl xl:text-6xl">Software Development Services for EdTech Start-ups</h2>
                     <h3 className="text-pitahaya-black w-10/12 ">We specialise in building bespoke learning platforms for EdTech Startups, harnessing modern technologies and AI.</h3>
                     <Link legacyBehavior href="/#contact" as="/#contact" >
                         <button className=" bg-pitahaya-black text-pitahaya-white px-4 py-2  rounded-md w-36 sm:w-56 md:w-56 h-14  hover:cursor-pointer transition duration-300 transform hover:scale-110">
